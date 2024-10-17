01fa00bd479ba9d839e51fa7d2c70dd6de8bdbcb
Uno-count <paonemorfi@gmail.com>
2024-10-17 23:05:01 +0800

    methods, wip

--- prices/methods.go
+++ prices/methods.go
@@ -1,6 +1,10 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func (job TaxIncludedPriceJob) Process() {
 	result := make(map[string]float64)
@@ -10,4 +14,31 @@ func (job TaxIncludedPriceJob) Process() {
 	}
 
 	fmt.Println(result)
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("data.txt")
+
+	if err != nil {
+		fmt.Println("An error occured")
+		fmt.Println(err)
+
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
 }
