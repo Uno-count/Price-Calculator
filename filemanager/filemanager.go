b5ddd1aa3862c9f43d804f6ec6e3313cc429720b
Uno-count <paonemorfi@gmail.com>
2024-11-01 07:01:49 +0800

    use defer for closing file

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -15,6 +15,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("failed to open file")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -25,11 +27,11 @@ func (fm FileManager) ReadLines() ([]string, error) {
 
 	err = scanner.Err()
 	if err != nil {
-		file.Close()
+		// file.Close()
 		return nil, errors.New("failed to read line in file")
 	}
 
-	file.Close()
+	// file.Close()
 
 	return lines, nil
 }
@@ -41,6 +43,8 @@ func (fm FileManager) WriteResult(data any) error {
 		return errors.New("failed to create file")
 	}
 
+	defer file.Close()
+
 	time.Sleep(3 * time.Second) //make up scenario for goroutine
 
 	encoder := json.NewEncoder(file)
@@ -50,7 +54,7 @@ func (fm FileManager) WriteResult(data any) error {
 		return errors.New("failed to convert data to json")
 	}
 
-	file.Close()
+	// file.Close()
 
 	return nil
 }
